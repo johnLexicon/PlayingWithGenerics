2ae95cdbeb367f5c4f5cff36d7ac12c8ee85335a
John Lundgren <john.paul.lundgren@gmail.com>
2019-02-10 10:22:18 +0100

    Made Buffer<T> class base class friendly.

--- Buffer.cs
+++ Buffer.cs
@@ -6,16 +6,16 @@ namespace PlayingWithGenerics
     public class Buffer<T> : IBuffer<T>
     {
 
-        private Queue<T> _queue = new Queue<T>();
+        protected Queue<T> _queue = new Queue<T>();
 
-        public bool IsEmpty => _queue.Count == 0;
+        public virtual bool IsEmpty => _queue.Count == 0;
 
-        public T Read()
+        public virtual T Read()
         {
             return _queue.Dequeue();
         }
 
-        public void Write(T value)
+        public virtual void Write(T value)
         {
             _queue.Enqueue(value);
         }
